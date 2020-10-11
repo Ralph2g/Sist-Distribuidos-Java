c99b6251ca360629c59320f3609a31d1d452ac62
Ralph2g <37715000+Ralph2g@users.noreply.github.com>
2020-10-11 01:19:55 -0500

    Se agrega la multiplicación de matricees y la transpuesta

--- 4.Mult-matrices-dist/src/main/java/com/mycompany/mult/matrices/dist/MultiplicaMatrices.java
+++ 4.Mult-matrices-dist/src/main/java/com/mycompany/mult/matrices/dist/MultiplicaMatrices.java
@@ -36,6 +36,21 @@ public class MultiplicaMatrices {
             longitud -=n;
         }
     }
+    //Metodo que multiplica 2 matrices y regresar una tercera 
+    static int[][] multiplicar(int[][] a, int[][] b, int[][] c){
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < b[0].length; j++)
+                for (int k = 0; k < a.length; k++)
+                    c[i][j] += a[i][k] * b[j][k];
+        return c;
+    }
+    static void checksum(){
+        int chck = 0;
+        for(int i =0;i!=N;i++)
+            for(int j =0;j!=N;j++)
+                chck += C[i][j];
+        System.out.println("El Checksum es: ".concat(Integer.toString(chck)));
+    }
     static class Worker extends Thread{
         Socket conexion;
         int nodo;
@@ -50,13 +65,40 @@ public class MultiplicaMatrices {
                 System.out.println("Se conectó el nodo: ".concat(Integer.toString(nodo)));
                 switch (nodo){
                     case 1:
+                        //Enviar matriz A1
+                        //Enviar matriz B1
                         
+                        //Recibir matriz C1
+
+                        //llenar Matriz C con c1
+
                         break;
                     case 2:
+                        //Enviar matriz A1
+                        //Enviar matriz B2
+                        
+                        //Enviar matriz C2
+
+                        //llenar Matriz C con c2
+
                         break;
                     case 3:
+                        //Enviar matriz A2
+                        //Enviar matriz B1
+                        
+                        //Recibir matriz C3
+
+                        //llenar Matriz C con c3
+
                         break;
                     case 4:
+                        //Enviar matriz A2
+                        //Enviar matriz B2
+                        
+                        //Recibir matriz C4
+                        
+                        //llenar Matriz C con c4
+
                         break;
                 }
                 //cerramos nuestros sockets y conexión
@@ -87,9 +129,31 @@ public class MultiplicaMatrices {
                     B[i][j] = 2 * i - j;
                     C[i][j] = 0;
                 }
+                // transpone la matriz B, la matriz traspuesta queda en B
+
+            for (int i = 0; i < N; i++)
+              for (int j = 0; j < i; j++)
+              {
+                int x = B[i][j];
+                B[i][j] = B[j][i];
+                B[j][i] = x;
+              }
             // Iniciamos nuestro servidor
             ServerSocket servidor = new ServerSocket(50000);
+            //Creamos nuestro Arreglo de Workers para nuestros clientes
+            Worker[] w = new Worker[4];
+            //Loop que limita el número de conexiones que aceptaremos
+            for (int i =0;i!=4;i++){
+                //Aceptamos la conexión
+                Socket conexion = servidor.accept();
+                //Instanciamos nuestra conexion
+                w[i] = new Worker(conexion);
+                //Inicializamos nuestro hilo
+                w[i].start();
+            }
+            
             //Calculamos Checksum de la matriz C
+            checksum();
             
             //Despliegue de datos segun los casos
             if (N<5){
@@ -107,6 +171,18 @@ public class MultiplicaMatrices {
         }else{
             //iniciamos la conexión del lado del cliente
             Socket conexion = null;
+            //Inicializamos Ax Bx y Cx
+            int[][] Ax = new int[N/2][N];
+            int[][] Bx = new int[N/2][N];
+            int[][] Cx = new int[N/2][N/2];
+            for(int i=0;i!=N/2;i++)
+                for(int j=0;j!=N;j++){
+                    Ax[i][j] = 0;
+                    Bx[i][j] = 0;
+                }
+            for(int i=0;i!=N/2;i++)
+                for(int j=0;j!=N/2;j++)
+                    Cx[i][j] = 0;
             //Realizamos el intento de conexion hasta que el servidor
             for(;;){
                 try{
@@ -116,12 +192,19 @@ public class MultiplicaMatrices {
                     Thread.sleep(100);
                 }
             }
+            System.out.println("Se logró conectar con el servidor correctamente");
             DataOutputStream salida = new DataOutputStream(conexion.getOutputStream());
             DataInputStream entrada = new DataInputStream(conexion.getInputStream());
             salida.writeInt(nodo);
             
-            System.out.println("Se logró conectar con el servidor correctamente");
+            //Rebimos Ax y Bx
+            Ax = Ax;
+            Bx = Bx;
+            //multiplicamos Ax y Bx y pbtenemos Cx para enviarlo al servidor
+            Cx = multiplicar(Ax,Bx,Cx);
+            //Enviamos Cx al servidor
             
+            //Cerramos nuestro cliente
             entrada.close();
             salida.close();
             conexion.close();
